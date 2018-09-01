e1f03a7290a6bf5b5497acd1f201474960a49e62
Shreenath Tewari <stewari.sps@gmail.com>
2018-09-01 22:03:46 +0530

    Vote component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Vote />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vote from './components/Vote.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Vote
   }
 }
 </script>
---
+++ src/components/Vote.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <h3>Choose the JavaScript Library/Framework of your Choice</h3>
+        <form @submit.prevent="castVote">
+            <input type="radio" value="react" v-model="framework"> React.js
+            <br>
+            <input type="radio" value="vue" v-model="framework"> Vue.js
+            <br>
+            <input type="radio" value="angular" v-model="framework"> Angular
+            <br>
+            <input type="submit">
+        </form>
+        {{ react }}<br>
+        {{ vue }}<br>
+        {{ angular }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Vote",
+        data() {
+            return {
+                framework: '',
+                react: 0,
+                angular: 0,
+                vue: 0
+            }
+        },
+        methods: {
+            castVote() {
+                if(this.framework == "react") {
+                    this.react++;
+                }
+                if(this.framework == "vue") {
+                    this.vue++;
+                }
+                if(this.framework == "angular") {
+                    this.angular++;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
